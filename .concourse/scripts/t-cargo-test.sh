72b30a00bdbab7240d626d459e71ee5db0ade146
Claus Strommer <claus@polymath.network>
2020-07-29 09:54:12 -0400

    update scripts

--- .concourse/scripts/t-cargo-test.sh
+++ .concourse/scripts/t-cargo-test.sh
@@ -7,11 +7,11 @@ set -o pipefail
 GIT_DIR=$1
 CACHE_DIR=$2
 
+# Restore the rust build cache
 mkdir -p ${CARGO_HOME:-$HOME/.cargo}
 mkdir -p ${GIT_DIR}/target
-
-rsync -auv ${CACHE_DIR}/.cargo/ ${CARGO_HOME:-$HOME/.cargo}
-rsync -auv ${CACHE_DIR}/target/ ${GIT_DIR}/target
+rsync -auv --size-only ${CACHE_DIR}/.cargo/ ${CARGO_HOME:-$HOME/.cargo}  | grep -e "^total size" -B1 --color=never
+rsync -auv --size-only ${CACHE_DIR}/target/ ${GIT_DIR}/target            | grep -e "^total size" -B1 --color=never
 
 cd $GIT_DIR
 
@@ -20,7 +20,7 @@ cargo test \
   --package pallet-staking \
   --package pallet-group \
   --package polymesh-primitives \
-  --package pallet-pips-rpc \
+  --package node-rpc-runtime-api \
   --package pallet-transaction-payment \
   --package pallet-cdd-offchain-worker \
   --features default_identity \
@@ -30,7 +30,7 @@ cargo test -j 1 \
   --package pallet-staking \
   --package pallet-group \
   --package polymesh-primitives \
-  --package pallet-pips-rpc \
+  --package node-rpc-runtime-api \
   --package pallet-transaction-payment \
   --package pallet-cdd-offchain-worker \
   --features default_identity \
